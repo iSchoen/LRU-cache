d35aa25032a99087363afe5c0673a96c741080a6
Ian Schoen <ian.rocha.schoen@gmail.com>
2022-07-08 13:24:27 -0600

    Fix typo

--- lruCache.js
+++ lruCache.js
@@ -17,7 +17,7 @@
  * cache.put(2, 2);
  * cache.get(1);       // returns 1
  * cache.put(3, 3);    // evicts key 2
- * cache.get(2);       // returns -1 (nt found)
+ * cache.get(2);       // returns -1 (not found)
  * cache.put(4, 4);    // evicts key 1
  * cache.get(1);       // returns -1 (not found)
  * cache.get(3);       // returns 3
